84ab4ddf216db5fcfd3cbd4f315c7a3e0406ae62
bkestelman <bkestelman@outlook.com>
2019-05-16 04:03:11 +0000

    check if container running on port

--- run.sh
+++ run.sh
@@ -5,12 +5,27 @@ fi
 SITE_NAME=$1
 PORT=${2:-8080} # default port 8080
 
+docker_build() {
+	docker build . -t $SITE_NAME
+}
+docker_run() {
+	docker run -it --rm -p ${PORT}:${PORT} --mount type=bind,source="$(pwd)",target=/www $SITE_NAME $PORT
+}
+docker_stop() {
+	docker stop --name $SITE_NAME
+}
+
 existing_images=$(( $(docker images $SITE_NAME | wc -l) - 1 )) # count docker images tagged $SITE_NAME (-1 to skip header)
 if [[ $existing_images == 0 ]]; then
 	echo "Building new image"
-	docker build . -t $SITE_NAME
+	docker_build
 else
 	echo "Using existing image"
 fi
 
-docker run -it --rm -p ${PORT}:${PORT} --mount type=bind,source="$(pwd)",target=/www $SITE_NAME $PORT
+running_containers=$(( $(docker ps --filter expose=$PORT | wc -l) - 1 )) # count running containers using $PORT (-1 to skip header)
+if [[ $running_containers > 0 ]]; then
+	echo "Container already running on $PORT."
+else
+	docker_run
+fi
